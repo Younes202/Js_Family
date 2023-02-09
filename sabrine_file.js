0ca62117630c9d12016881c11c171d6d45a0c5bf
sabrine0 <124470900+sabrine0@users.noreply.github.com>
2023-02-09 21:59:16 +0100

    EX15:
    ECRIRE 4 FONCTIONS: ADDITION, SOUSTRACTION, PRODUIT ET DIVISION.
    CHACUNE DOIT PRENDRE EN PARAMETRE 2 NOMBRES.
    ECRIRE UNE 5IEME FONCTION NOMMEE "OPERATION" QUI DOIT PRENDRE 3 PARAMETRES : 2 NOMBRES, ET
    UN CARACTERE
    QUI SERA + - * OU /. EN FONCTION DE LA VALEUR DU CARACTERE, CETTE FONCTION VA APPELER L'UNE
    DES
    4 AUTRES FONCTIONS, ET RETOURNER SON RESULTAT. TRAITER LES CAS D'ERREUR SUIVANTS:
    - L'UN DES NOMBRES EST INCORRECTE (CONTIENT DES LETTRES PAR EXP.) (OPERATEUR "TYPEOF")
    - LE CARACTERE EST INCORRECTE.
    LA DEMANDE DES PARAMETRES ET LE RESULTAT DOIVENT ETRE AFFICHES DANS LE PROGRAMME PRINCIPAL

--- sabrine_file.js
+++ sabrine_file.js
@@ -1,1 +1,40 @@
+function ADDITION(a, b) { //+
+    return a + b;
+}
+function SOUSTRACTION(a, b) { //-
+    return a - b;
+}
+function PRODUIT(a, b) { //*
+    return a * b;
+}
+function DIVISION(a, b) {  //"/"
+    if (b == 0) {
+        return "maymknch t9sm 3la 0";
+    }
+    return a / b;
+}
+function OPERATEUR(a, b, c) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return "Error: Invalid number";
+    }
+    switch (c) {
+        case '+':
+            return ADDITION(a, b);
+        case '-':
+            return SOUSTRACTION(a, b);
+        case '*':
+            return PRODUIT(a, b);
+        case '/':
+            return DIVISION(a, b);
+        default:
+            return "Error OPERATEUR";
+    }
+}
+let a = parseFloat(prompt("Enter number 1: "));
+let b = parseFloat(prompt("Enter number 2: "));
+let c = prompt("Enter +, -, *, / : ");
+
+let result = OPERATEUR(a, b, c);
+
+console.log(" result : " + result);
 
