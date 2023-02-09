b81f32cc8b5123e020d06e82e5cf91ce35b12400
arif1h <124675242+arif1h@users.noreply.github.com>
2023-02-09 21:50:33 +0100

    Update arif_file.js

--- arif_file.js
+++ arif_file.js
@@ -1,1 +1,41 @@
+function Addition(n1,n2){
+    return n1+n2
+}
+function SOUSTRACTION(n1,n2){
+    return n1-n2
+}
+function PRODUIT(n1,n2){
+    return n1*n2
+}
+function DIVISION(n1,n2){
+    return n1/n2
+}
 
+
+
+function opeartion(x,y,z){
+    if(z=="+"){
+        console.log("la somme de",x,"et",y,"est",Addition(x,y))
+    }else if(z=="-"){
+        console.log("la SOUSTRACTION de",x,"et",y,"est",SOUSTRACTION(x,y))
+    
+    }
+    else if(z=="*"){
+        console.log("la PRODUIT de",x,"et",y,"est",PRODUIT(x,y))
+    
+    }
+    else if(z=="/"){
+        console.log("la DIVISION de",x,"et",y,"est",DIVISION(x,y))
+    
+    }
+
+
+
+}
+
+
+num1=parseInt(prompt("donnez la valeur 1"))
+num2=parseInt(prompt("donnez la valeur 2"))
+op=prompt("donnez opearation")
+
+opeartion(num1,num2,op)
