eb7d8616d96d1b56e7ee3db2142608d841348a54
arif1h <124675242+arif1h@users.noreply.github.com>
2023-02-12 02:04:12 +0100

    Update arif_file.js

--- arif_file.js
+++ arif_file.js
@@ -63,7 +63,86 @@ console.log(PLUS_GRAND(tba))
 
 
 
+///EX4
 
+function produit(de,qt,pu){
+    this.désignation=de
+    this.qte_commandee=qt
+    this.prix_unitaire=pu
+
+    this.total_p=function(){
+        return this.qte_commandee*this.prix_unitaire    
+    }
+
+}
+
+function DateC(j,m,y){
+    this.jour =j;
+    this.mois=m;
+    this.annee = y;
+}
+
+function commande(idc,date_c,li){
+    this.id_commande=idc,
+    this.date_commande=date_c
+    this.liste_des_produits=li
+
+}
+
+tab_produit=[]
+
+x=prompt("donnez le nombre produit ")
+
+for(let i=0;i<x;i++){
+    A=prompt("donnez la  désignation ")
+    B=prompt("donnez la  qte_commandee ")
+    c=prompt("donnez le prix ")
+
+
+    pro=new produit(A,B,c)
+
+    tab_produit.push(pro)
+     
+
+
+    
+    
+
+
+
+}
+especes= prompt("versement espèces ")
+total_pa=0
+
+document.write("************FACTURE*******</th>")
+
+for(let i=0;i<tab_produit.length;i++){
+
+        
+
+
+        document.write("<br>"+"**PRODUIT "+(i+1)+"*****")
+        document.write("<br>"+"   Design :  "+tab_produit[i].désignation+"<br>")  
+        document.write("<br>"+"   Prix Unit : "+tab_produit[i].qte_commandee+"<br>")  
+        document.write("<br>"+"   QTE :     "+tab_produit[i].prix_unitaire+"<br>")  
+       to=tab_produit[i].prix_unitaire*tab_produit[i].qte_commandee
+       document.write("<br>"+"   Total Produit :     "+to+"<br>") 
+       total_pa+=to
+       rendu=especes-to
+   
+
+
+        
+
+
+}
+
+document.write("---------------------------------------------------------------------") 
+
+document.write("<br>"+"   Total A PAYER :     "+total_pa+"<br>") 
+ 
+document.write("<br>"+"         ESPECES :     "+especes+"<br>")  
+document.write("<br>"+"           RENDU :     "+rendu+"<br>")  
 
 
 
